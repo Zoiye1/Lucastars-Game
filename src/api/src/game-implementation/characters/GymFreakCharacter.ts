f8ef6c14f728c0614f8903159fbdc71d57e78204
Mustafa Jeylani <mustafa.jeylani@hva.nl>
2025-02-25 01:27:32 +0100

    interactie met GYMFREAK

--- src/api/src/game-implementation/characters/GymFreakCharacter.ts
+++ src/api/src/game-implementation/characters/GymFreakCharacter.ts
@@ -1,4 +1,7 @@
 import { ActionResult } from "../../game-base/actionResults/ActionResult";
+import { TalkActionResult } from "../../game-base/actionResults/TalkActionResult";
+import { TextActionResult } from "../../game-base/actionResults/TextActionResult";
+import { TalkChoice } from "../../game-base/actions/TalkAction";
 import { Character } from "../../game-base/gameObjects/Character";
 
 export class GymFreakCharacter extends Character {
@@ -13,6 +16,36 @@ export class GymFreakCharacter extends Character {
     }
 
     public talk(choiceId?: number): ActionResult | undefined {
-        return undefined;
+        if (choiceId === 1) {
+            return new TalkActionResult(
+                this,
+                [
+                    "I could help you, but I need something from you.",
+                    "If you could give me steroids, then I will help you escape.",
+                ],
+                [
+                    new TalkChoice(3, "I have steroids, take it."),
+                    new TalkChoice(4, "I'm gonna search for it."),
+                ]
+            );
+        }
+
+        else if (choiceId === 2 || choiceId === 4) {
+            return new TextActionResult(["Okay...",
+                "I will continue working out.",
+            ]);
+        }
+
+        return new TalkActionResult(
+            this,
+            [
+                "Hi, im very busy training",
+                "What do you want from me?",
+            ],
+            [
+                new TalkChoice(1, "Can you help me escape?"),
+                new TalkChoice(2, "Oh.. Sorry to bother you"),
+            ]
+        );
     }
 }
