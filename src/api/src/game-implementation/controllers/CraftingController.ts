c6d8e0e46d974159a533a441b57d99d0c59e207a
Farmaan Mohammad <farmaan.mohammad@hva.nl>
2025-03-14 17:44:46 +0100

    chore(craftingcontroller): tSDOCS toegeveogd aan controller

--- src/api/src/game-implementation/controllers/CraftingController.ts
+++ src/api/src/game-implementation/controllers/CraftingController.ts
@@ -3,7 +3,15 @@ import { gameService } from "../../global";
 import { GameController } from "./GameController";
 import { Request, Response } from "express";
 
+/**
+ * Controller voor het afhandelen van alle crafting verzoeken
+ */
 export class CraftingController extends GameController {
+    /**
+     * Verwerkt het verzoek om een item op te halen voor de speler
+     *
+     * @remarks Response is een 200 met een bevestigingsbericht bij succes
+     */
     public handleRetrieveRequest(req: Request, res: Response): void {
         const executeRetrieveRequest: ExecuteRetrieveRequest = req.body as ExecuteRetrieveRequest;
 
@@ -11,6 +19,11 @@ export class CraftingController extends GameController {
         res.status(200).json({ message: result });
     }
 
+    /**
+     * Verwerkt het verzoek om items te verwijderen voor de speler
+     *
+     * @remarks Response is een 200 met een bevestigingsbericht bij succes
+     */
     public handleDeleteItemsRequest(req: Request, res: Response): void {
         const executeDeleteItemsRequest: ExecuteDeleteItemsRequest = req.body as ExecuteDeleteItemsRequest;
 
@@ -18,6 +31,13 @@ export class CraftingController extends GameController {
         res.status(200).json({ message: result });
     }
 
+    /**
+     * Voert de actie uit om een item op te halen en toe te voegen aan de inventory
+     *
+     * @param itemAlias Alias van het item dat moet worden opgehaald
+     *
+     * @returns Een bevestigingsbericht dat aangeeft of de actie succesvol was
+     */
     private executeRetrieveItem(itemAlias: string): string {
         const inventory: string[] = gameService.getPlayerSession().inventory;
         if (!inventory.includes(itemAlias)) {
@@ -29,6 +49,13 @@ export class CraftingController extends GameController {
         }
     }
 
+    /**
+     * Voert de actie uit om items te verwijderen uit de inventory
+     *
+     * @param itemsToDelete Array van item aliases die moeten worden verwijderd
+     *
+     * @returns Een bevestigingsbericht dat aangeeft of de actie succesvol was
+     */
     private executeDeleteItems(itemsToDelete: string[]): string {
         const inventory: string[] = gameService.getPlayerSession().inventory;
 
