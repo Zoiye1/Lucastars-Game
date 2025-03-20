91ce0c2ff472cf1ec4b4e5ad601db4dbfd3d9f76
Zoiye van Leeuwen <zoiyevanleeuwen@icloud.com>
2025-03-20 14:38:36 +0100

    Feat: Lab-Ending Finished

---
+++ src/api/src/game-implementation/items/CorrosiveAcidItem.ts
@@ -0,0 +1,34 @@
+import { GameObjectType } from "../../game-base/gameObjects/GameObject";
+import { Item } from "../../game-base/gameObjects/Item";
+
+/**
+ * Class die het item "Ladder" representeert
+ *
+ * Dit item kan worden geplaatst (Place).
+ */
+export class CorrosiveAcidItem extends Item {
+    /** Alias die wordt gebruikt om dit item te identificeren */
+    public static readonly Alias: string = "CorrosiveAcid";
+
+    public constructor() {
+        super(CorrosiveAcidItem.Alias);
+    }
+
+    /**
+     * Geeft de naam van het item terug
+     *
+     * @returns De naam van het item
+     */
+    public name(): string {
+        return "CorrosiveAcid";
+    }
+
+    /**
+     * Geeft de type van de GameObject terug
+     *
+     * @returns De type van de GameObject (GameObjectType union)
+     */
+    public type(): GameObjectType[] {
+        return ["actionableItem"];
+    }
+}
